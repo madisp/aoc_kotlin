a7864fe4e857d07ead6a6d90513d8c74b4c3452c
Madis Pink <mail@madis.pink>
2021-12-19 17:31:13 +0200

    Add a better product

--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -26,6 +26,32 @@ fun <T> Iterable<T>.withLCounts(): Map<T, Long> {
   return groupBy { it }.mapValues { (_, c) -> c.size.toLong() }
 }
 
+operator fun <T> List<T>.times(other: List<T>): List<Pair<T, T>> {
+  return flatMap { first ->
+    other.map { second ->
+      first to second
+    }
+  }
+}
+
+fun <T> List<T>.product(other: List<T>): List<Pair<T, T>> = this * other
+
+inline fun <T, V> List<T>.product(other: List<T>, fn: (v1: T, v2: T) -> V): List<V> {
+  return flatMap { first ->
+    other.map { second ->
+      fn(first, second)
+    }
+  }
+}
+
+inline fun <T, V> List<T>.productIndexed(other: List<T>, fn: (i1: Int, v1: T, i2: Int, v2: T) -> V): List<V> {
+  return flatMapIndexed { i1, v1 ->
+    other.mapIndexed { i2, v2 ->
+      fn(i1, v1, i2, v2)
+    }
+  }
+}
+
 fun <T> List<T>.product(): List<Pair<T, T>> {
   return flatMapIndexed { index, first ->
     subList(index + 1, size).map { second ->
@@ -34,11 +60,15 @@ fun <T> List<T>.product(): List<Pair<T, T>> {
   }
 }
 
-fun <T, V> List<T>.product(fn: (Pair<T, T>) -> V): List<V> {
-  return product().map { fn(it) }
+inline fun <T, V> List<T>.product(fn: (v1: T, v2: T) -> V): List<V> {
+  return flatMapIndexed { index, first ->
+    subList(index + 1, size).map { second ->
+      fn(first, second)
+    }
+  }
 }
 
-fun <T, V> List<T>.productIndexed(fn: (i1: Int, v1: T, i2: Int, v2: T) -> V): List<V> {
+inline fun <T, V> List<T>.productIndexed(fn: (i1: Int, v1: T, i2: Int, v2: T) -> V): List<V> {
   return flatMapIndexed { i1, v1 ->
     subList(i1 + 1, size).mapIndexed { i2, v2 ->
       fn(i1, v1, i1 + 1 + i2, v2)
