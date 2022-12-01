525f0212802569c8ab0080eb788aeddb3b3dd98f
Madis Pink <mail@madis.pink>
2022-12-01 18:08:12 +0200

    Add a generic base class for benchmarking

--- 2022/src/jmh/kotlin/bench/Day1.kt
+++ 2022/src/jmh/kotlin/bench/Day1.kt
@@ -3,31 +3,13 @@ package bench
 import Day1Fast
 import Day1Func
 import Day1Imp
-import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.annotations.Scope
 import org.openjdk.jmh.annotations.State
-import org.openjdk.jmh.infra.Blackhole
-import utils.readFile
 
+typealias Func = List<List<Int>>
+typealias Imp = Func
+typealias Fast = List<IntArray>
+
+@Suppress("unused")
 @State(Scope.Benchmark)
-open class Day1 {
-  private val inputString = readFile("day1")
-  private val funcInput = Day1Func.parser(inputString)
-  private val impInput = Day1Imp.parser(inputString)
-  private val fastInput = Day1Fast.parser(inputString)
-
-  @Benchmark
-  fun benchmarkFuncPart2(bh: Blackhole) {
-    bh.consume(Day1Func.part2(funcInput))
-  }
-
-  @Benchmark
-  fun benchmarkImpPart2(bh: Blackhole) {
-    bh.consume(Day1Imp.part2(impInput))
-  }
-
-  @Benchmark
-  fun benchmarkFastPart2(bh: Blackhole) {
-    bh.consume(Day1Fast.part2(fastInput))
-  }
-}
+open class Day1 : DayBench<Func, Imp, Fast>("day1", Day1Func, Day1Imp, Day1Fast)
---
+++ 2022/src/jmh/kotlin/bench/DayBench.kt
@@ -0,0 +1,33 @@
+package bench
+
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.infra.Blackhole
+import utils.Solution
+import utils.readFile
+
+abstract class DayBench<Func, Imp, Fast>(
+  day: String,
+  private val func: Solution<Func>,
+  private val imp: Solution<Imp>,
+  private val fast: Solution<Fast>
+) {
+  private val input = readFile(day)
+  private val funcInput = func.parser(input)
+  private val impInput = imp.parser(input)
+  private val fastInput = fast.parser(input)
+
+  @Benchmark
+  fun benchmarkFuncPart2(bh: Blackhole) {
+    bh.consume(func.part2(funcInput))
+  }
+
+  @Benchmark
+  fun benchmarkImpPart2(bh: Blackhole) {
+    bh.consume(imp.part2(impInput))
+  }
+
+  @Benchmark
+  fun benchmarkFastPart2(bh: Blackhole) {
+    bh.consume(fast.part2(fastInput))
+  }
+}
