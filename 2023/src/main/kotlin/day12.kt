43db75f32ee446c689f959084f141b3cbb282b8f
Madis Pink <mail@madis.pink>
2023-12-12 14:18:35 +0200

    Add Memo utility

--- 2023/src/main/kotlin/day12.kt
+++ 2023/src/main/kotlin/day12.kt
@@ -1,3 +1,4 @@
+import utils.Memo
 import utils.Parse
 import utils.Parser
 import utils.Solution
@@ -22,50 +23,37 @@ object Day12 : Solution<List<Day12.Input>>() {
       if (map.substring(index, index + group).any { it == '.' }) return null
       if (index + group < map.length && map[index + group] == '#') return null
 
-      val ret = Input(map.substring(minOf(index + group + 1, map.length)), dmgGroups.drop(1))
-
-      return ret
+      return Input(map.substring(minOf(index + group + 1, map.length)), dmgGroups.drop(1))
     }
   }
 
-  private val arrangeCache = mutableMapOf<Input, Long>()
-
-  private fun countArrangements(input: Input): Long {
-    val cached = arrangeCache[input]
-    if (cached != null) {
-      return cached
-    }
-
+  private val countArrangements = Memo<Input, Long> { input ->
     val firstNonDotChar = input.map.indexOfFirst { it != '.' }
 
     if (input.dmgGroups.isEmpty()) {
-      return if (input.map.none { it == '#' }) 1L else 0L
+      return@Memo if (input.map.none { it == '#' }) 1L else 0L
     } else if (input.map.isEmpty() || firstNonDotChar == -1) {
-      return if (input.dmgGroups.isEmpty()) 1L else 0L
+      return@Memo if (input.dmgGroups.isEmpty()) 1L else 0L
     }
 
     // check whether we can place a group
-    val nextWithPlaced = input.place(firstNonDotChar)?.let { countArrangements(it) } ?: 0L
+    val nextWithPlaced = input.place(firstNonDotChar)?.let { this(it) } ?: 0L
 
     // if first char is not '#' then we can also not place a group
     val nextWithoutPlaced = if (input.map[firstNonDotChar] != '#') {
-      countArrangements(Input(input.map.substring(firstNonDotChar + 1), input.dmgGroups))
+      this(Input(input.map.substring(firstNonDotChar + 1), input.dmgGroups))
     } else 0L
 
-    return (nextWithPlaced + nextWithoutPlaced).also { arrangeCache[input] = it }
+    return@Memo (nextWithPlaced + nextWithoutPlaced)
   }
 
   override fun part1(): Long {
-    val arrangements = input.map { countArrangements(it) }
-    countArrangements(input.last())
-    return arrangements.sum()
+    return input.sumOf { countArrangements(it) }
   }
 
   override fun part2(): Long {
     return input
-      .map { line ->
-        Input(List(5) { line.map }.joinToString("?"), List(5) { line.dmgGroups }.flatten())
-      }
+      .map { line -> Input(List(5) { line.map }.joinToString("?"), List(5) { line.dmgGroups }.flatten()) }
       .sumOf { countArrangements(it) }
   }
 }
