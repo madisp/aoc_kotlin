9d650f8aadc26e545cc7ab2f03c7a049c888aea8
Madis Pink <mail@madis.pink>
2023-12-16 08:29:28 +0200

    Split day 16 into solution and fast

--- 2023/src/main/kotlin/day16.kt
+++ 2023/src/main/kotlin/day16.kt
@@ -1,8 +1,9 @@
 import utils.Grid
-import utils.MutableIntGrid
 import utils.Parser
 import utils.Solution
 import utils.Vec2i
+import utils.createMutableGrid
+import java.util.EnumSet
 
 fun main() {
   Day16.run()
@@ -12,92 +13,93 @@ object Day16 : Solution<Grid<Char>>() {
   override val name = "day16"
   override val parser = Parser.charGrid
 
-  private val directions = listOf(Vec2i.UP, Vec2i.DOWN, Vec2i.LEFT, Vec2i.RIGHT)
-
-  private const val UP = 0
-  private const val DOWN = 1
-  private const val LEFT = 2
-  private const val RIGHT = 3
-
-  private val cw = listOf(RIGHT, LEFT, UP, DOWN)
-  private val ccw = listOf(LEFT, RIGHT, DOWN, UP)
-
   data class Beam(
     val location: Vec2i,
-    val direction: Int,
+    val direction: Direction,
   )
+
+  enum class Direction(val delta: Vec2i) {
+    UP(Vec2i(0, -1)),
+    DOWN(Vec2i(0, 1)),
+    LEFT(Vec2i(-1, 0)),
+    RIGHT(Vec2i(1, 0));
+
+    companion object {
+      fun byVec(v: Vec2i): Direction {
+        return Direction.entries.first { it.delta == v }
+      }
+    }
+  }
 
   private fun solve(start: Beam): Int {
     val beams = mutableListOf(start)
-    val seen = MutableIntGrid(IntArray(input.width * input.height), input.width, input.height)
+    val seen = createMutableGrid<Set<Direction>>(input.width, input.height) { EnumSet.noneOf(Direction::class.java) }
 
     while (beams.isNotEmpty()) {
       val beam = beams.removeLast()
-      val dir = directions[beam.direction]
-      val nextLocation = beam.location + dir
+      val nextLocation = beam.location + beam.direction.delta
 
       if (nextLocation.x !in 0 until input.width || nextLocation.y !in 0 until input.height) {
         // out of bounds
         continue
       }
 
-      if ((1 shl beam.direction) and seen[nextLocation] != 0) {
+      if (beam.direction in seen[nextLocation]) {
         // already tracked a beam going that way
         continue
       }
 
-
-      seen[nextLocation] = seen[nextLocation] or (1 shl beam.direction)
+      seen[nextLocation] = seen[nextLocation] + beam.direction
       val newBeams = when (input[nextLocation]) {
         '.' -> listOf(Beam(nextLocation, beam.direction))
-        '-' -> if (dir.y == 0) {
+        '-' -> if (beam.direction.delta.y == 0) {
           listOf(Beam(nextLocation, beam.direction))
         } else {
-          listOf(Beam(nextLocation, LEFT), Beam(nextLocation, RIGHT))
+          listOf(Beam(nextLocation, Direction.LEFT), Beam(nextLocation, Direction.RIGHT))
         }
-        '|' -> if (dir.x == 0) {
+        '|' -> if (beam.direction.delta.x == 0) {
           listOf(Beam(nextLocation, beam.direction))
         } else {
-          listOf(Beam(nextLocation, UP), Beam(nextLocation, DOWN))
+          listOf(Beam(nextLocation, Direction.UP), Beam(nextLocation, Direction.DOWN))
         }
-        '\\' -> if (dir.y == 0) {
-          listOf(Beam(nextLocation, cw[beam.direction]))
+        '\\' -> if (beam.direction.delta.y == 0) {
+          listOf(Beam(nextLocation, Direction.byVec(beam.direction.delta.rotateCcw())))
         } else {
-          listOf(Beam(nextLocation, ccw[beam.direction]))
+          listOf(Beam(nextLocation, Direction.byVec(beam.direction.delta.rotateCw())))
         }
-        '/' -> if (dir.y == 0) {
-          listOf(Beam(nextLocation, ccw[beam.direction]))
+        '/' -> if (beam.direction.delta.y == 0) {
+          listOf(Beam(nextLocation, Direction.byVec(beam.direction.delta.rotateCw())))
         } else {
-          listOf(Beam(nextLocation, cw[beam.direction]))
+          listOf(Beam(nextLocation, Direction.byVec(beam.direction.delta.rotateCcw())))
         }
         else -> throw IllegalStateException("Bad grid char ${input[beam.location]}")
       }
       beams.addAll(newBeams)
     }
 
-    return seen.values.count { it != 0 }
+    return seen.values.count { it.isNotEmpty() }
   }
 
   override fun part1(): Int {
-    return solve(Beam(Vec2i(-1, 0), RIGHT))
+    return solve(Beam(Vec2i(-1, 0), Direction.RIGHT))
   }
 
-  override fun part2(): Int {
-    val startLocations = listOf(
+  override fun part2(): Any? {
+    val entries = listOf(
       (0 until input.height).flatMap {
         listOf(
-          Beam(Vec2i(-1, it), RIGHT),
-          Beam(Vec2i(input.width, it), LEFT)
+          Beam(Vec2i(-1, it), Direction.RIGHT),
+          Beam(Vec2i(input.width, it), Direction.LEFT)
         )
       },
       (0 until input.width).flatMap {
         listOf(
-          Beam(Vec2i(it, -1), DOWN),
-          Beam(Vec2i(it, input.height), UP)
+          Beam(Vec2i(it, -1), Direction.DOWN),
+          Beam(Vec2i(it, input.height), Direction.UP)
         )
       },
     ).flatten()
 
-    return startLocations.maxOf { solve(it) }
+    return entries.maxOf { solve(it) }
   }
 }
---
+++ 2023/src/main/kotlin/day16_fast.kt
@@ -0,0 +1,106 @@
+import utils.Grid
+import utils.MutableIntGrid
+import utils.Parser
+import utils.Solution
+import utils.Vec2i
+
+fun main() {
+  Day16Fast.run()
+}
+
+object Day16Fast : Solution<Grid<Char>>() {
+  override val name = "day16"
+  override val parser = Parser.charGrid
+
+  private val directions = listOf(Vec2i.UP, Vec2i.DOWN, Vec2i.LEFT, Vec2i.RIGHT)
+
+  private const val UP = 0
+  private const val DOWN = 1
+  private const val LEFT = 2
+  private const val RIGHT = 3
+
+  private val cw = listOf(RIGHT, LEFT, UP, DOWN)
+  private val ccw = listOf(LEFT, RIGHT, DOWN, UP)
+
+  private fun solve(start: Int): Int {
+    val beams = IntArray(128)
+    beams[0] = start
+    var beamsSize = 1
+    val seen = MutableIntGrid(IntArray(input.width * input.height), input.width, input.height)
+    var energizedCount = 0
+
+    while (beamsSize > 0) {
+      val beam = beams[--beamsSize]
+
+      val direction = beam and 0b11
+      val x = (beam ushr 2 and 0xff) - 1
+      val y = (beam ushr 10 and 0xff) - 1
+      val location = Vec2i(x, y)
+
+      val dir = directions[direction]
+      val nextLocation = location + dir
+
+      if (nextLocation.x !in 0 until input.width || nextLocation.y !in 0 until input.height) {
+        // out of bounds
+        continue
+      }
+
+      val seenAtNext = seen[nextLocation]
+      if (seenAtNext == 0) {
+        energizedCount++
+      } else if ((1 shl direction) and seenAtNext != 0) {
+        // already tracked a beam going that way
+        continue
+      }
+
+      val nextLocationPacked = ((nextLocation.x + 1) shl 2) or ((nextLocation.y + 1) shl 10)
+
+      seen[nextLocation] = seen[nextLocation] or (1 shl direction)
+      when (input[nextLocation]) {
+        '.' -> beams[beamsSize++] = nextLocationPacked or direction
+        '-' -> if (dir.y == 0) {
+          beams[beamsSize++] = nextLocationPacked or direction
+        } else {
+          beams[beamsSize++] = nextLocationPacked or LEFT
+          beams[beamsSize++] = nextLocationPacked or RIGHT
+        }
+        '|' -> if (dir.x == 0) {
+          beams[beamsSize++] = nextLocationPacked or direction
+        } else {
+          beams[beamsSize++] = nextLocationPacked or UP
+          beams[beamsSize++] = nextLocationPacked or DOWN
+        }
+        '\\' -> if (dir.y == 0) {
+          beams[beamsSize++] = nextLocationPacked or cw[direction]
+        } else {
+          beams[beamsSize++] = nextLocationPacked or ccw[direction]
+        }
+        '/' -> if (dir.y == 0) {
+          beams[beamsSize++] = nextLocationPacked or ccw[direction]
+        } else {
+          beams[beamsSize++] = nextLocationPacked or cw[direction]
+        }
+        else -> throw IllegalStateException("Bad grid char ${input[location]}")
+      }
+    }
+
+    return energizedCount
+  }
+
+  override fun part1(): Int {
+    return solve((1 shl 10) or RIGHT)
+  }
+
+  override fun part2(): Int {
+    val inputs = mutableListOf<Int>()
+    for (y in 0 until input.height) {
+      inputs += (y + 1 shl 10) or RIGHT
+      inputs += (input.width + 1 shl 2) or (y + 1 shl 10) or RIGHT
+    }
+    for (x in 0 until input.width) {
+      inputs += (x + 1 shl 2) or DOWN
+      inputs += (x + 1 shl 2) or (input.height + 1 shl 10) or DOWN
+    }
+    return inputs.maxOf { solve(it) }
+  }
+}
