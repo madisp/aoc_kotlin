dead04593c38b39e17daffa5ae7e0bb77102b203
Madis Pink <mail@madis.pink>
2023-12-18 14:50:10 +0200

    Add day18 fast

---
+++ 2023/src/main/kotlin/day18_fast.kt
@@ -0,0 +1,50 @@
+import utils.Parser
+import utils.Solution
+import utils.Vec2l
+import utils.mapItems
+import kotlin.math.absoluteValue
+
+fun main() {
+  Day18Fast.run()
+}
+
+object Day18Fast : Solution<List<Day18.Line>>() {
+  override val name = "day18"
+  override val parser = Parser.lines.mapItems { Day18.parseLine(it) }
+
+  private fun solve(insns: List<Day18.Instruction>): Long {
+    var y = 0L
+    var pts = 0L
+    var area = 0L
+    for (i in insns) {
+      area += i.direction.x * i.len * y
+      y += (i.direction.y * i.len)
+      pts += i.len
+    }
+    return area.absoluteValue + (pts / 2) + 1
+  }
+
+  override fun part1(): Long {
+    val insns = input.map { line ->
+      val direction = when (line.direction) {
+        "R" -> Vec2l.RIGHT
+        "D" -> Vec2l.UP
+        "U" -> Vec2l.DOWN
+        "L" -> Vec2l.LEFT
+        else -> throw IllegalStateException("Unknown direction char ${line.direction}")
+      }
+      Day18.Instruction(direction, line.len)
+    }
+    return solve(insns)
+  }
+
+  override fun part2(): Long {
+    val directions = listOf(Vec2l.RIGHT, Vec2l.DOWN, Vec2l.LEFT, Vec2l.UP)
+    val corrected = input.map { line ->
+      val direction = directions[line.color.last() - '0']
+      val len = line.color.substring(0, line.color.length - 1).toLong(16)
+      Day18.Instruction(direction, len)
+    }
+    return solve(corrected)
+  }
+}
