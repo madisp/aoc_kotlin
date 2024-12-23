cd730cccb9fd03da6a704ba333b823948624d906
Madis Pink <mail@madis.pink>
2024-12-23 07:34:55 +0200

    Add Day 23

---
+++ 2024/src/main/kotlin/day23.kt
@@ -0,0 +1,75 @@
+import utils.Parser
+import utils.Solution
+import utils.badInput
+import utils.cut
+import utils.mapItems
+import utils.selections
+
+fun main() {
+  Day23.run(skipPuzzle = false)
+}
+
+typealias Day23In = List<Pair<String, String>>
+
+object Day23 : Solution<Day23In>() {
+  override val name = "day23"
+  override val parser: Parser<Day23In> = Parser.lines.mapItems { it.cut("-") }
+
+  override fun part1(input: Day23In): Int {
+    val connected = mutableMapOf<Pair<String, String>, Boolean>()
+    val computers = mutableSetOf<String>()
+    input.forEach { (a, b) ->
+      connected[a to b] = true
+      connected[b to a] = true
+      computers += a
+      computers += b
+    }
+
+    val answ = mutableSetOf<String>()
+
+    input.forEach { (a, b) ->
+      if (a.startsWith("t") || b.startsWith("t")) {
+        computers.forEach {
+          if (it != a && it != b && (connected[it to a] == true) && (connected[it to b] == true)) {
+            answ += listOf(a, b, it).sorted().joinToString(",")
+          }
+        }
+      }
+    }
+
+    return answ.size
+  }
+
+  override fun part2(input: Day23In): String {
+    val connected = mutableMapOf<Pair<String, String>, Boolean>()
+    val computers = mutableMapOf<String, MutableSet<String>>()
+
+    input.forEach { (a, b) ->
+      connected[a to b] = true
+      connected[b to a] = true
+      computers.getOrPut(a) { mutableSetOf() }.add(b)
+      computers.getOrPut(b) { mutableSetOf() }.add(a)
+    }
+
+    val maxSize = computers.maxOf { it.value.size }
+
+    for (sz in maxSize downTo 3) {
+      val groupComputers = computers.filter { it.value.size == sz }.keys
+      groupComputers.forEach { one ->
+        val others = computers[one]!!.toList()
+        if (others.size >= maxSize - 1) {
+          others.selections(maxSize - 1).forEach { selection ->
+            val fits = selection.count {
+              computers[it]!!.containsAll(selection - it + one)
+            } >= (maxSize - 1)
+            if (fits) {
+              return (selection + one).toList().sorted().joinToString(",")
+            }
+          }
+        }
+      }
+    }
+
+    badInput()
+  }
+}
---
+++ 2024/src/test/kotlin/day23.kt
@@ -0,0 +1,7 @@
+import utils.SolutionTest
+
+class Day23Test : SolutionTest<Day23In>(
+  solution = Day23,
+  examples = 7 to "co,de,ka,ta",
+  answers = 1215 to "bm,by,dv,ep,ia,ja,jb,ks,lv,ol,oy,uz,yt",
+)
