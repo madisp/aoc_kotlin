7939190c9760799ed1e9ce3a780d75862458e9d1
Madis Pink <mail@madis.pink>
2024-12-25 09:12:43 +0200

    Clean up Day 25 a bit

--- 2024/src/main/kotlin/day25.kt
+++ 2024/src/main/kotlin/day25.kt
@@ -1,4 +1,9 @@
-import utils.*
+import utils.Grid
+import utils.Parser
+import utils.Solution
+import utils.map
+import utils.mapItems
+import utils.times
 
 fun main() {
   Day25.run()
@@ -8,15 +13,13 @@ typealias Day25In = List<Grid<Char>>
 
 object Day25 : Solution<Day25In>() {
   override val name = "day25"
-  override val parser: Parser<Day25In> = Parser { it.trim().split("\n\n") }
-    .mapItems { Parser.charGrid(it) }
+  override val parser: Parser<Day25In> = Parser { it.trim().split("\n\n") }.mapItems { Parser.charGrid(it) }
 
-  override fun part1(input: Day25In): Any? {
-    val (lockGrids, keyGrids) = input.partition { g -> g.getRow(0).values.all { it == '#' } }
+  private val Grid<Char>.pinHeights get() = columns.map { c -> c.values.count { it == '#' } - 1 }
+  private val Grid<Char>.isLock get() = getRow(0).values.all { it == '#' }
 
-    val locks = lockGrids.map { g -> g.columns.map { c -> c.values.count { it == '#' } - 1 } }
-    val keys = keyGrids.map { g -> g.columns.map { c -> c.values.count { it == '#' } - 1 } }
-
+  override fun part1(input: Day25In): Int {
+    val (locks, keys) = input.partition { it.isLock }.map { list -> list.map { it.pinHeights } }
     return (locks * keys).map { (lock, key) -> lock.zip(key).map { (a, b) -> a + b } }
       .count { sum -> sum.all { it <= 5 } }
   }
